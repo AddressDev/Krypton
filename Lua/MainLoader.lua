f1e59f1562e2536f1ed3ccc807cb9af807ebf598
Address Dev <Address.Dev.Business@gmail.com>
2025-03-16 18:27:59 +0900

    Update MainLoader.lua

--- Lua/MainLoader.lua
+++ Lua/MainLoader.lua
@@ -1,1 +1,11 @@
-game.Players.LocalPlayer:Kick("Not Fully Released")
+local games_id = {
+    [5859134979] = 'https://raw.githubusercontent.com/AddressDev/Krypton/refs/heads/main/Lua/games/KoreanMiddleSchool.lua', -- 일찐중학교
+    [5720801512] = 'https://raw.githubusercontent.com/AddressDev/Krypton/refs/heads/main/Lua/games/MurderMysteryKorean.lua', -- 한국머더
+}
+
+local url = games_id[game.PlaceId]
+if url then
+    loadstring(game:HttpGet(url))()
+else
+    game:GetService("Players").LocalPlayer:Kick("\n[Krypton] Not Supported Game!")
+end
